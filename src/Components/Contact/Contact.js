3d35b217328e24e55acf159436246ddd7f5d1183
tohinahmedrahat <rahat4248@gmail.com>
2023-02-14 23:19:50 +0600

    add about

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import "./Contact.css";
+const Contact = () => {
+    return (
+        <div>
+            <div className='page-header'>
+                <div className='page-header__bg'>
+                </div>
+                <div className='container'>
+                    <h6 className='text-white text-2xl font-semibold ml-4'>Home / Contact</h6>
+                    <h5 className='text-white text-5xl mt-1 font-semibold ml-4'>Contact</h5>
+                </div>
+            </div>
+            <div className='mt-8'>
+                <div className='bg-[#F9F6FF] md:flex w-[90%] mx-auto'>
+                    <div className='p-20'>
+                        <h4 className='text-[#9c2ede] font-bold text-base'>Contact With Us</h4>
+                        <h2 className='text-5xl font-bold mt-1'>Join Us To Get IT Free <br /> Consultations</h2>
+                        <form action="" className='mt-12 custom-form'>
+                            <input className='h-[56px] pl-7 mr-3 w-[295px]' type="text" placeholder='Your Name' />
+                            <input className='h-[56px] pl-7 w-[295px]' type="email" placeholder='Your Email' /><br />
+                            <textarea className='mt-4 pl-7 pt-5' placeholder='Write Message' cols="59" rows="8"></textarea>
+                            <button type="submit" className='btn-grad'>Send A Message</button>
+                        </form>
+                    </div>
+                    <div className='contact-aside'>
+                        <div className='relative'>
+                            <h3 className='text-white font-bold text-lg uppercase tracking-widest'>Address</h3>
+                            <p className='text-[#CBB8F6] mt-1 leading-7'>88 New Street, Washington DC
+                               <br /> United States, America</p>
+                        </div>
+                        <div className='relative mt-9'>
+                            <h3 className='text-white font-bold text-lg uppercase tracking-widest'>Phone</h3>
+                            <p className='text-[#CBB8F6] mt-1 leading-7'>Phone: <span className='contact-one__item__text'>+8801317-713199</span>
+                               <br /> phone: <span>+8801867-585183</span></p>
+                        </div>
+                        <div className='relative mt-9'>
+                            <h3 className='text-white font-bold text-lg uppercase tracking-widest'>Email</h3>
+                            <p className='text-[#CBB8F6] mt-1 leading-7'>88 New Street, Washington DC
+                               <br /> United States, America</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Contact;
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -20,39 +20,32 @@ const Navbar = () => {
             console.log('n')
         }
     };
+    const menuItem = <React.Fragment>
+        <li><Link class="nav-link text-gray-400" to="/">Home <ArrowForwardIosIcon className='nav-icon'></ArrowForwardIosIcon></Link></li>
 
+        <li><Link class="nav-link  text-gray-400" to="/about">About Us <ArrowForwardIosIcon className='nav-icon'></ArrowForwardIosIcon></Link></li>
+
+        <li><Link class="nav-link text-gray-400" to="/service">Services <ArrowForwardIosIcon className='nav-icon'></ArrowForwardIosIcon></Link></li>
+
+        <li><Link class="nav-link text-gray-400" to="/project">Project <ArrowForwardIosIcon className='nav-icon'></ArrowForwardIosIcon></Link></li>
+        <li><Link class="nav-link text-gray-400" to="/contact">Contact <ArrowForwardIosIcon className='nav-icon'></ArrowForwardIosIcon></Link></li>
+    </React.Fragment>
 
 
     return (
         <div class="sticky top-0 z-50 ">
             <nav class=" px-4 py-4 flex justify-between items-center bg-[#41d7eb28] bg-[#0EE29C] bg-nav " id='header'>
-                <a class="text-3xl font-bold leading-none flex items-center text-[#fffdfd]" href="#">
+                <Link class="text-3xl font-bold leading-none flex items-center text-[#fffdfd]" to="/">
                     RainboSoft
-                </a>
+                </Link>
                 <ul class="hidden  md:flex lg:mx-auto md:flex md:items-center lg:w-auto lg:space-x-6">
-                    <li><a class="nav-link text-gray-400  " href="#">Home <ArrowForwardIosIcon className='nav-icon'></ArrowForwardIosIcon></a></li>
-
-                    <li><Link class="nav-link  text-gray-400  " to="/about">About Us <ArrowForwardIosIcon className='nav-icon'></ArrowForwardIosIcon></Link></li>
-
-                    <li><a class="nav-link text-gray-400  " href="#">Services <ArrowForwardIosIcon className='nav-icon'></ArrowForwardIosIcon></a></li>
-
-                    <li><a class="nav-link text-gray-400  " href="#">Project <ArrowForwardIosIcon className='nav-icon'></ArrowForwardIosIcon></a></li>
-                    <li><a class="nav-link text-gray-400  " href="#">Contact <ArrowForwardIosIcon className='nav-icon'></ArrowForwardIosIcon></a></li>
+                    {menuItem}
                 </ul>
                 <button className='bg-[#ffffff93] px-5 py-3 text-[#000000] font-bold uppercase rounded-sm md:block hidden'>Get A Quote</button>
                 <button onClick={() => setOpen(!open)} className='md:hidden text-white'><MenuIcon ></MenuIcon></button>
             </nav>
             <ul className={`${open ? 'left-0' : 'left-[-1200px]'} absolute w-full ease-in duration-700 md:hidden`}>
-                <li><a class="nav-link-2 bg-[#E02156] w-full px-4 py-2 block" href="#">Home <ArrowForwardIosIcon className='nav-icon'></ArrowForwardIosIcon></a></li>
-                <hr />
-
-                <li><a class="nav-link-2  text-gray-400   bg-[#E02156] px-4 py-2 block" href="#">About Us <ArrowForwardIosIcon className='nav-icon'></ArrowForwardIosIcon></a></li>
-                <hr />
-                <li><a class="nav-link-2 text-gray-400   bg-[#E02156] px-4 py-2 block" href="#">Services <ArrowForwardIosIcon className='nav-icon'></ArrowForwardIosIcon></a></li>
-                <hr />
-                <li><a class="nav-link-2 text-gray-400   bg-[#E02156] px-4 py-2 block" href="#">Project <ArrowForwardIosIcon className='nav-icon'></ArrowForwardIosIcon></a></li>
-                <hr />
-                <li><a class="nav-link-2 text-gray-400   bg-[#E02156] px-4 py-2 block" href="#">Contact <ArrowForwardIosIcon className='nav-icon'></ArrowForwardIosIcon></a></li>
+                {menuItem}
             </ul>
         </div>
     );
