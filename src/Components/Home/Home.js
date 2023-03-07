f5bbc0f3ad81fa6ee42c2e1e0cc9d8ea52800214
tohinahmedrahat <rahat4248@gmail.com>
2023-03-07 12:00:37 +0600

    update home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
-import Footer from '../Footer/Footer';
 import HighPerformence from '../HighPerformence/HighPerformence';
 import Management from '../Management/Management';
-import OurProject from '../Ourproject/Ourproject';
 import OurTeam from '../OurTeam/OurTeam';
 import Pricing from '../Priceing/Priceing';
 import Technology from '../Technology/Technology';
-import Testimonial from '../Testimonial/Testimonial';
 import TransformSection from '../TransformSection/TransformSection';
 import WhyChose from '../WhyChose/whyChose';
 
@@ -20,14 +17,6 @@ const Home = () => {
             <Pricing></Pricing>
             <Management></Management>
             <WhyChose></WhyChose>
-
-        
-            <OurProject></OurProject>
-            <Testimonial></Testimonial>
-            {/* <OurTeam></OurTeam> *
-            {/* <About></About> */}
-            {/* <OurProject></OurProject> */}
-            {/* <Testimonial></Testimonial> */}
             <Technology></Technology>
             <OurTeam></OurTeam>
         </div>
